9e8c263a1e73a23c67c6797bcafb760b7bf7f897
S04tQlick <sammieackun@hotmail.com>
2025-10-06 18:14:47 +0100

    pages refactored

---
+++ lib/sanity/config/client-config.ts
@@ -0,0 +1,9 @@
+import {createClient} from "next-sanity";
+
+const config = createClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_PROJECT_VERSION,
+});
+
+export default config;
---
+++ lib/sanity/config/index.ts
@@ -0,0 +1,11 @@
+import pageSchema from "@/lib/sanity/schemas/page-schema";
+import imageAssetSchema from "@/lib/sanity/schemas/image-asset-schema";
+
+//import project from "@/lib/sanity/schemas/project-schema";
+//import assetCategory from "@/lib/sanity/schemas/asset-category-schema";
+
+
+
+const schemas = [ pageSchema, imageAssetSchema ];
+
+export default schemas;
